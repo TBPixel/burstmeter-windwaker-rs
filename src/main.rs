bc42a69a0688af348cbb4334a017777a0d271331
Madison Barry <madison.barry@hey.com>
2021-11-27 09:37:58 -0800

    v0.1.3 - Improve stability, game detection and better handle edge cases where burst may not apply when it should have

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use crossterm::cursor;
 use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::style::Print;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use dolphin_memory::Dolphin;
 use std::io::stdout;
 
 use std::thread;
@@ -17,8 +18,8 @@ fn main() -> anyhow::Result<()> {
     let dolphin = loop {
         if let Ok(p) = dolphin_memory::Dolphin::new() {
             // it takes a moment to initialize ram,
-            // so let's just wait a second here before we check game version
-            thread::sleep(Duration::from_secs(1));
+            // so let's just wait a couple seconds here before we check game version
+            thread::sleep(Duration::from_secs(3));
 
             if !windwaker::gcm::is_supported_wind_waker(&p) {
                 return Err(anyhow!("Unsupported game or version of TLoZ: The Wind Waker! This must be the NA GameCube release!"));
@@ -58,9 +59,10 @@ fn main() -> anyhow::Result<()> {
             mp.current >= 2 && Instant::now().duration_since(last_burst).as_millis() > 200;
 
         if input.dpad_left_just_pressed && can_burst {
+            if let Ok(_) = charge_magic_cost(&mut mp, 2, &dolphin) {
+                burst(1600.0, &dolphin);
+            }
             last_burst = Instant::now();
-            mp.write_current(mp.current - 2, &dolphin)?;
-            windwaker::player::Speed::default().write(1600.0, &dolphin)?;
         }
 
         // matching the key
@@ -82,4 +84,28 @@ fn main() -> anyhow::Result<()> {
     disable_raw_mode()?;
 
     Ok(())
+}
+
+// charge_magic_cost is a helper function that consumes the given amount of magic
+// or returns an error.
+fn charge_magic_cost(
+    mp: &mut windwaker::player::Mp,
+    amount: u8,
+    d: &Dolphin,
+) -> anyhow::Result<()> {
+    if mp.current < amount {
+        return Err(anyhow!("not enough magic!"));
+    }
+
+    if let Err(_) = mp.write_current(mp.current - amount, d) {
+        return charge_magic_cost(mp, amount, d);
+    }
+
+    Ok(())
+}
+
+fn burst(amount: f32, d: &Dolphin) {
+    if let Err(_) = windwaker::player::Speed::default().write(amount, d) {
+        return burst(amount, d);
+    }
 }
